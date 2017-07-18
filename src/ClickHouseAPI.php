05598139967cbe04a1eed4aadfc6a76d5134f723
ierusalim <alex@ierusalim.com>
2017-07-19 00:59:54 +0300

    phpdoc correcting

--- src/ClickHouseAPI.php
+++ src/ClickHouseAPI.php
@@ -4,11 +4,17 @@ namespace ierusalim\ClickHouse;
 
 /**
  * This class contains simple http/https connector for ClickHouse db-server
+ *
  * No dependency, nothing extra.
  *
  * PHP Version >= 5.4
  *
  * This file is a part of packet php-clickhouse, but it may be used independently.
+ *
+ * @package    ierusalim/php-clickhouse
+ * @author     Alexander Jer <alex@ierusalim.com>
+ * @copyright  2017, Ierusalim
+ * @license    https://opensource.org/licenses/Apache-2.0 Apache-2.0
  *
  * Example of independent use:
  *
@@ -22,51 +28,46 @@ namespace ierusalim\ClickHouse;
  *  $data = json_decode($data['response']);
  *  $ch->postQuery("DROP TABLE t");
  *  print_r($data);
- *
- * @package    ierusalim/php-clickhouse
- * @author     Alexander Jer <alex@ierusalim.com>
- * @copyright  2017, Ierusalim
- * @license    https://opensource.org/licenses/Apache-2.0 Apache-2.0
  */
 class ClickHouseAPI
 {
     /**
-     * http or https
+     * Protocol for access to ClickHouse server
      *
-     * @var string
+     * @var string http or https
      */
     public $scheme = 'http';
-    
+
     /**
-     * Server IP or host name
+     * ClickHouse server IP or host name
      *
      * @var string
      */
     public $host = '127.0.0.1';
-    
+
     /**
-     * Server TCP/IP-port
+     * ClickHouse server TCP/IP-port
      *
      * @var integer
      */
     public $port = 8123;
-    
+
     /**
      * Base path for server request
      *
      * @var string
      */
     public $path = '/';
-    
+
     /**
-     * Username if need authorization
+     * Username if need authorization on ClickHouse server
      *
      * @var string|null
      */
     private $user;
-    
+
     /**
-     * Password if need authorization
+     * Password if need authorization on ClickHouse server
      *
      * @var string|null
      */
@@ -78,28 +79,28 @@ class ClickHouseAPI
      * @var type string
      */
     private $server_url;
-    
+
     /**
      * CURL option for do not verify hostname in SSL certificate
      *
-     * @var integer
+     * @var integer 0 by default. Set 2 if server have valid ssl-sertificate
      */
-    public $ssl_verify_host = 0; //set 2 if server have valid ssl-sertificate
-    
+    public $ssl_verify_host = 0;
+
     /**
      * CURL option CURLOPT_TIMEOUT
      *
-     * @var integer
+     * @var integer 30 sec. by default
      */
     public $curl_timeout = 30;
-    
+
     /**
      * Last error reported by CURL or empty string if none
      *
      * @var string
      */
     public $last_curl_error_str = '';
-    
+
     /**
      * HTTP-Code from last server response
      *
@@ -113,35 +114,35 @@ class ClickHouseAPI
      * @var array
      */
     public $options = [];
-    
+
     /**
      * Set true for show sending request and server answers
      *
      * @var boolean
      */
     public $debug = false;
-    
+
     /**
      * Last sent query or default query when not specified
      *
      * @var string
      */
     public $query = 'SELECT 1';
-    
+
     /**
      * Hook on doApiCall executing, before send request.
      *
      * @var callable|boolean
      */
     public $hook_before_api_call = false;
-    
+
     /**
      * Auto-create session_id and send it with each request
      *
      * @var boolean
      */
     public $session_autocreate = true;
-    
+
     /**
      * Last used session_id
      *
@@ -151,19 +152,21 @@ class ClickHouseAPI
 
     /**
      * Two formats supported for set server parameters when creating object:
+     *
      *  $h = new ClickHouseAPI("http://127.0.0.1:8123/" [, $user, $pass]);
-     * or
+     *
      *  $h = new ClickHouseAPI("127.0.0.1", 8321 [, $user, $pass]);
      *
      * Also, server parameters may be set late via setServerUrl($url)
+     *
      * Example:
      *  $h = new ClickHouseAPI;
      *  $h->setServerUrl("https://user:pass@127.0.0.1:8443/");
      *
-     * @param string|null $host_or_full_url
-     * @param string|null $port
-     * @param string|null $user
-     * @param string|null $pass
+     * @param string|null $host_or_full_url Host name or Full server URL
+     * @param string|null $port TCP-IP port of ClickHouse server
+     * @param string|null $user user for authorization (if need)
+     * @param string|null $pass password for authorization (if need)
      */
     public function __construct(
         $host_or_full_url = null,
@@ -189,14 +192,17 @@ class ClickHouseAPI
         }
         $this->setServerUrl();
     }
-    
+
     /**
      * Set server connection parameters from url
+     *
      * Example:
+     *
      * Set scheme=http, host=127.0.0.1, port=8123, user=default, pass=[empty]
+     *
      *  $h->setServerUrl("http://default:@127.0.0.1:8123/");
      *
-     * @param string|null $full_server_url
+     * @param string|null $full_server_url Full server URL
      * @throws \Exception
      */
     public function setServerUrl($full_server_url = null)
@@ -220,13 +226,13 @@ class ClickHouseAPI
             . ':' . $this->port
             . (empty($this->path) ? '/' : $this->path);
     }
-    
+
     /**
      * Send Get query if $post_data is empty, otherwise send Post query
      *
-     * @param string            $h_query
-     * @param array|string|null $post_data
-     * @param string|null       $sess
+     * @param string            $h_query Parameters send in http-request after "?"
+     * @param array|string|null $post_data Parameters send in request body
+     * @param string|null       $sess session_id
      * @return array
      */
     public function anyQuery($h_query, $post_data = null, $sess = null)
@@ -236,7 +242,7 @@ class ClickHouseAPI
             $this->getQuery($h_query, $sess) : $this->postQuery($h_query, $post_data, $sess)
         ;
     }
-    
+
     /**
      * Send Get API-query
      *
@@ -248,7 +254,7 @@ class ClickHouseAPI
     {
         return $this->doQuery($h_query, false, null, $sess);
     }
-    
+
     /**
      * Send Post API-query
      *
@@ -266,15 +272,15 @@ class ClickHouseAPI
     ) {
         return $this->doQuery($h_query, true, $post_fields, $sess, $file);
     }
-    
+
     /**
-     * Send Get or Post API-query depends of $is_post value
+     * Send Get or Post API-query depends of $is_post parameter
      *
-     * @param string $query
-     * @param boolean $is_post
-     * @param array|string|null $post_fields
-     * @param string|null $session_id
-     * @param string|null $file
+     * @param string $query SQL-query for send to ClickHouse server
+     * @param boolean $is_post true for send POST-request, false for GET
+     * @param array|string|null $post_fields for send in POST-request body
+     * @param string|null $session_id session_id
+     * @param string|null $file file name (full name with path) for send
      * @return array
      */
     public function doQuery(
@@ -292,7 +298,7 @@ class ClickHouseAPI
 
         $user = $this->user;
         $password = $this->pass;
-        
+
         // Set session if need
         if (!empty($session_id) && $this->getSession() != $session_id) {
             $old_session = $this->setSession($session_id);
@@ -301,15 +307,15 @@ class ClickHouseAPI
                 $this->setSession();
             }
         }
-        
+
         $h_parameters = \array_merge(
             \compact('user', 'password', 'query'),
             $this->options
         );
-        
+
         $this->last_used_session_id = isset($h_parameters['session_id']) ?
             $h_parameters['session_id'] : null;
-        
+
         $response_data = $this->doApiCall(
             $this->server_url,
             $h_parameters,
@@ -322,37 +328,37 @@ class ClickHouseAPI
         if (!empty($old_session)) {
             $this->setSession($old_session);
         }
-                
+
         return $response_data;
     }
-    
+
     /**
      * Send API query on server and get answer
      *
-     * @param string $api_url
-     * @param array $get_params
-     * @param boolean $post_mode
-     * @param array|string|null $post_fields
-     * @param string|null $file
+     * @param string $api_url Full URL of server
+     * @param array $h_params Parameter after "?"
+     * @param boolean $post_mode true for POST request, false for GET request
+     * @param array|string|null $post_fields Data for send in body of POST-request
+     * @param string|null $file file name (full name with path) for send
      * @return array
      */
     public function doApiCall(
         $api_url,
-        $get_params,
+        $h_params,
         $post_mode = false,
         $post_fields = null,
         $file = null
     ) {
-        $api_url .= "?" . \http_build_query($get_params);
+        $api_url .= "?" . \http_build_query($h_params);
 
         if ($this->hook_before_api_call) {
             $api_url = call_user_func($this->hook_before_api_call, $api_url);
         }
-        
+
         if ($this->debug) {
             echo ($post_mode ? 'POST' : 'GET') . "->$api_url\n" . $file;
         }
-        
+
         $ch = curl_init($api_url);
 
         if ($post_mode) {
@@ -395,10 +401,10 @@ class ClickHouseAPI
     /**
      * Set addition http-request parameter.
      *
-     * @param string $key
-     * @param string|null $value
-     * @param boolean $overwrite
-     * @return string|null
+     * @param string $key option name
+     * @param string|null $value option value
+     * @param boolean $overwrite true = set always, false = set only if not defined
+     * @return string|null Return old value (or null if old value undefined)
      */
     public function setOption($key, $value, $overwrite = true)
     {
@@ -408,25 +414,25 @@ class ClickHouseAPI
         }
         return $old_value;
     }
-    
+
     /**
      * Get http-request parameter that was set via setOption
      *
-     * @param string $key
-     * @return string|null
+     * @param string $key option name
+     * @return string|null Return option value or null if option not defined
      */
     public function getOption($key)
     {
         return isset($this->options[$key]) ? $this->options[$key] : null;
     }
-    
+
     /**
      * Set session_id into http-request options
      * if session_id not specified (or specified as null) create and set random.
      *
-     * @param string|null $session_id
-     * @param boolean $overwrite
-     * @return string|null
+     * @param string|null $session_id session_id or null for generate new id
+     * @param boolean $overwrite true = set only if session not defined, false = always
+     * @return string|null Return old value of session_id option
      */
     public function setSession($session_id = null, $overwrite = true)
     {
@@ -435,7 +441,7 @@ class ClickHouseAPI
         }
         return $this->setOption('session_id', $session_id, $overwrite);
     }
-    
+
     /**
      * Return current session_id from http-req options (or null if not exists)
      *
@@ -445,12 +451,12 @@ class ClickHouseAPI
     {
         return $this->getOption('session_id');
     }
-    
+
     /**
      * Delete http-request option by specified key
      *
-     * @param string $key
-     * @return string|null
+     * @param string $key Option name
+     * @return string|null Return old value of deleted option
      */
     public function delOption($key)
     {
